10937415495222fdcc46a9d5b6dd1562d5c580e2
Robert Maples <robert.a.maples@gmail.com>
2022-03-28 10:08:25 -0400

    created command line game

--- main.go
+++ main.go
@@ -28,5 +28,36 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	fmt.Println(adventure)
+	playChapter(adventure, adventure["intro"])
+}
+
+func playChapter(a map[string]chapter, c chapter) {
+	fmt.Printf("\n\n%s\n\n", c.Title)
+	for _, line := range c.Story {
+		fmt.Printf("\t%s\n\n", line)
+	}
+
+	if len(c.Options) == 0 {
+		os.Exit(0)
+	}
+
+	for i, o := range c.Options {
+		fmt.Printf("%v.) %s\n", i+1, o.Text)
+	}
+
+	var input int
+	_, err := fmt.Scanf("%d", &input)
+	if err != nil {
+		// TODO: make prompt repeating for incorrect input
+		log.Fatalln(err)
+	}
+
+	index := input - 1
+	if index < 0 || index >= len(c.Options) {
+		log.Fatalln("Input must be one of the available options.")
+	}
+
+	arc := c.Options[index].Arc
+
+	playChapter(a, a[arc])
 }
